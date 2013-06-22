1d18a3c56f167db66145d32a1b1ccdc4723d2868
DrJonOsterman <egarcia2388@gmail.com>
2013-06-22 13:31:28 -0400

    routing set up

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-var http = require("http");
+// var http = require("http");
 
-function serverFunc(request, response)
-{
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write('Hello World');
-	response.end();
-}
-http.createServer(serverFunc).listen(process.env.PORT || 3000);
+
+// var html = '<!DOCTYPE html><html><head></head><body><h2>ok, this  is being served as text/html now.</h2></body></html>';
+
+// function serverFunc(request, response)
+// {
+// 	response.writeHead(200, {"Content-Type": "text/html"});
+// 	response.write(html);
+// 	console.log(__dirname);
+// 	response.end();
+// }
+// http.createServer(serverFunc).listen(process.env.PORT || 3000);
+
+
+var connect = require("connect");
+var connectRt = require("connect-route");
+var app = connect();
+
+
+app.use(connectRt(function(router){
+
+	router.get('/', function(req, res, next){
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.write('<h1>This the home yall.<a href="/update">Testing request</a></h1>');
+		res.end(); //res.end('index');
+	});
+
+		router.get('/update', function(req, res, next){
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.write('<h1>Request works</h1>');
+		res.end(); //res.end('index');
+	});
+
+}));
+
+app.listen(process.env.PORT || 3000);
