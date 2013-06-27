e4dd5729826caebf3d49c3e8ffa928bba7d1557b
DrJonOsterman <egarcia2388@gmail.com>
2013-06-27 09:20:38 -0400

    cleaning up files

--- server.js
+++ server.js
@@ -2,17 +2,11 @@ var connect = require("connect");
 var connectRt = require("connect-route");
 var app = connect();
 
-
-//IN THE SECOND MESSAGE IT'S JUST SUPPOSED TO RETURN ONE, BUT IT RETURNS FIRST AND SECOND, WHCIHC MESESUP THE SCRIPT
-
 var indexHTML = "<!DOCTYPE html><html><head><title>Chat Room</title><style type=\"text/css\">html, body{width: 100%; height: 100%;}.message{display: block; background-color: gray;}</style><script type=\"text/javascript\" src=\"http://code.jquery.com/jquery-1.10.1.min.js\"></script></head><body><script type=\"text/javascript\">var chatJS= {insert: function(contextElem){var txt = document.createElement('div');txt.setAttribute('id', 'txtArea');txt.style.width = '100%';txt.style.height = '75%';var tb = document.createElement('input');tb.setAttribute('id', 'txtMssg');tb.setAttribute('type', 'text');tb.style.width = '80%';tb.style.height = '20%';tb.style.verticalAlign = 'top';var bt = document.createElement('button');bt.setAttribute('id', 'btnSnd');bt.innerHTML = 'SEND';bt.style.width = '19%';bt.style.height = '20%';contextElem.appendChild(txt);contextElem.appendChild(tb);contextElem.appendChild(bt);},send: function(){	var mssgNum = ($('#txtArea').is(':empty')) ? -1 : parseInt($('#txtArea span:last').attr('data-num'), 10);$.post('/post', {sn:'Human#1', message : $('#txtMssg').val(), last:mssgNum}).done(function(data){console.log(data);temp = JSON.parse(data);temp.forEach(function(sms){$('#txtArea').append('<span class=\"message\" data-num=\"' + sms.num + '\">' + sms.sn + ': ' + sms.text + '</span>');});});}};chatJS.insert(document.getElementsByTagName('body')[0]);$('#btnSnd').click(chatJS.send);</script></body></html>";
 
-
-
-var posts = [];// {'sn':'spooky', 'text':'behind you'},
+var posts = [];//Array of objects. use {'sn':'spookyGhost', 'text':'look behind you'},
 
 function getPostsInRange(start){
-
 	var tempObj = [];
 	var ii = 0;
 	for (var i = start, l = posts.length; i < l ; i++)
@@ -20,7 +14,6 @@ function getPostsInRange(start){
 		tempObj.push(posts[i]);
 		tempObj[ii].num = i;
 		ii += 1;
-
 	}
 	return JSON.stringify(tempObj);
 }
@@ -40,22 +33,15 @@ app.use(connectRt(function(router){
 
 	router.post('/post', function(req, res, next){
 		posts.push({'sn': req.body.sn, 'text':req.body.message});
-		console.log('Posts object');console.log(posts);
-		console.log('Request');	console.log(req.body);
-
 		res.writeHead(200, {"Content-Type": "text/plain"});
-		console.log('Response');
 		if (req.body.last === '-1')
 		{
-			console.log(JSON.stringify([{'sn': req.body.sn, 'text':req.body.message, 'num':0}]));
 			 res.end(JSON.stringify([{'sn': req.body.sn, 'text':req.body.message, 'num':0}]));
 		}
 		else
 		{
-			console.log(getPostsInRange(parseInt(req.body.last, 10) + 1));
 			res.end(getPostsInRange(parseInt(req.body.last, 10) + 1));
 		}
-	console.log('-------------------------------------------------')
 	});
 
 	router.post('/join', function(req, res, next){
@@ -67,7 +53,7 @@ app.use(connectRt(function(router){
 
 app.listen(process.env.PORT || 3000);
 
-//There is no way to implement a real chatroom with http requests only
+//There is no way to implement a real chatroom with http requests
 //The simulation will have to be making frequent requests to the server
 //Client makes post
 //server takes post and gives it post number
@@ -75,5 +61,3 @@ app.listen(process.env.PORT || 3000);
 //on response to post, bring back the latest x posts
 //have client-code matchup which posts are already on screen
 //client sends request with latest post
-
-//Ruby parser. Turn html file into javascript string;
